e23b103a31b3ab7d2c063da259cb66d85351496f
nitayk <k.nitay@gmail.com>
2014-05-27 20:49:51 +0300

    final ver

--- assignment3/crash_please.d
+++
@@ -1,2 +0,0 @@
-crash_please.o: crash_please.c /usr/include/stdc-predef.h types.h stat.h \
- user.h fs.h fcntl.h
--- assignment3/testTask3.d
+++
@@ -1,2 +0,0 @@
-testTask3.o: testTask3.c /usr/include/stdc-predef.h types.h stat.h \
- param.h uthread.h user.h
